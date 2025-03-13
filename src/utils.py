fdcb6d47514b4bd8fcc4ee643c32dd6e237a982e
aj <mohamad.hasan.sohan.ajini@gmail.com>
2025-03-13 03:44:27 +0330

    Implement am detector

--- src/utils.py
+++ src/utils.py
@@ -159,7 +159,9 @@ def get_amd_record(dialed_number):
 def detect_answering_machine(call: Call) -> None:
     """Detect answering machine."""
     logger = get_logger()
-    call_id = call.getInfo().callIdString
+    call_info = call.getInfo()
+    call_id = call_info.callIdString
+    dialed_number = get_number(call_info.remoteUri)
     logger.info(f"Call ID: {call_id}")
 
     # audio recorder
@@ -184,24 +186,63 @@ def detect_answering_machine(call: Call) -> None:
             time.sleep(0.01)
     fs = wav_info.samplerate
 
-    # each packet appended every 100-120 ms (jitter absolutely possible!)
+    # gather first few seconds of the call
+    # Note: each packet appended every 100-120 ms (jitter absolutely possible!)
     sad = None
     t0 = time.time()
     while time.time() - t0 < Algorithm.max_call_duration:
         if sad is None:
             sad = SAD()
         time.sleep(0.01)
+
+    # process audio data
     appended_bytes = wav_file.read()
     audio_buffer = parse_new_frames(appended_bytes, wav_info)
     zero_buffer = np.zeros(Algorithm.zero_padding, dtype=np.float32)
     audio_buffer = np.concatenate((zero_buffer, audio_buffer, zero_buffer))
     data = convert_np_array_to_wav_file_bytes(audio_buffer, fs)
     sad_result = sad.handle([data])[0]
+
+    # create metadata dict
+    metadata_dict = {
+        "call_id": call_id,
+        "dialed_number": dialed_number,
+        "result": "",
+    }
+
+    # check if SAD detects any speech signal
+    if len(sad_result) == 0:
+        logger.warning("No speech detected!")
+        metadata_dict["result"] = "non-AMD"
+        metadata_dict["asr_result"]
+        metadata_dict["duration"] = time.time() - t0
+        return metadata_dict
+    start_sample = int(sad_result[0]["start"] * fs)
+    end_sample = int(sad_result[-1]["end"] * fs)
+    audio_segment = audio_buffer[start_sample:end_sample]
+    data = convert_np_array_to_wav_file_bytes(audio_segment, fs)
+
     # ASR (non blocking)
+    process = spawn_background_am_asr(data, call_id)
+
     # fetch history
+    old_amd_record = get_amd_record(dialed_number)
+
+    # retrieve ASR result
+    while process.is_alive():
+        time.sleep(0.01)
+    am_insertion_time, am, asr_result = recover_am_asr(call_id)
+
     # asr string matching
-    # audio pattern matching
-    return
+    metadata_dict["asr_result"] = asr_result
+    if old_amd_record and old_amd_record.asr_result == asr_result:
+        metadata_dict["result"] = "AMD"
+    else:
+        metadata_dict["result"] = "non-AMD"
+
+    # TODO: keyword spotting
+    # TODO: audio pattern matching
+    return metadata_dict
 
 
 def store_wav(file_path):
