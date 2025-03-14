57612178a11b94e904ffc69553efb2aef1b9336e
aj <mohamad.hasan.sohan.ajini@gmail.com>
2025-03-14 23:55:58 +0330

    Remove date

--- services/keep_amd_alive.sh
+++ services/keep_amd_alive.sh
@@ -4,7 +4,7 @@ After=network.target
 
 [Service]
 Type=simple
-ExecStart=/bin/bash -c 'log_file="/root/answering-machine-detection/log/log-01--$(date +%Y-%m-%d--%H:%M:%S)--$(uuidgen).txt" && source /root/.bashrc && /root/venv/bin/python /root/answering-machine-detection/src/user_agent.py --domain "127.0.0.1" --src-user "8501" --src-pass "pass8501" --always > "$log_file" 2>&1'
+ExecStart=/bin/bash -c 'log_file="/root/answering-machine-detection/log/log-01-$(uuidgen).txt" && source /root/.bashrc && /root/venv/bin/python /root/answering-machine-detection/src/user_agent.py --domain "127.0.0.1" --src-user "8501" --src-pass "pass8501" --always > "$log_file" 2>&1'
 Restart=always
 RestartSec=3
 Environment="PATH=/usr/local/bin:/usr/bin"
