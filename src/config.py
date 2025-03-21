ed092d612fb539000ea116f8a715c220234beec1
aj <mohamad.hasan.sohan.ajini@gmail.com>
2025-03-21 04:42:58 +0330

    Increase the tail silence

--- src/config.py
+++ src/config.py
@@ -52,7 +52,7 @@ class UserAgent:
 class Algorithm:
     max_call_duration: float = 15.0
     zero_padding: int = 4000
-    max_tail_sil: float = 0.5
+    max_tail_sil: float = 0.7
     am_rft: float = 1 / 10
     asr_decoder_rtf: float = 1 / 10
     kws_rtf: float = 1 / 12
