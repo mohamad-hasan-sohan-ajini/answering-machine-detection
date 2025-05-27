37d26ce1ee3261d5e1ebdafa15c91b760f3efa3c
aj <mohamad.hasan.sohan.ajini@gmail.com>
2025-05-28 00:16:47 +0330

    Alter callbacks

--- src/custom_callbacks.py
+++ src/custom_callbacks.py
@@ -6,42 +6,40 @@ import pjsua2 as pj
 class Call(pj.Call):
     def __init__(self, acc, call_id=pj.PJSUA_INVALID_ID):
         super().__init__(acc, call_id)
-        self.info_changed = False
-        self.media_changed = False
-        self.last_info = None
-        self.last_prm = None
+        self._delete_call = False
+        self._media_consented = False
 
     def onCallState(self, prm):
         """Invoked with call state changes in SIP."""
-        self.info_changed = True
-        self.last_prm = prm
-        return super().onCallState(prm)
+        ci = self.getInfo()
+        if ci.state == pj.PJSIP_INV_STATE_DISCONNECTED:
+            self._delete_call = True
 
     def onCallMediaState(self, prm):
         """Invoked upon call media consent between caller and callee."""
-        self.media_changed = True
-        self.last_prm = prm
+        # self.media_changed = True
+        # self.last_prm = prm
+        print("media state changed\n" * 100)
+        ci = self.getInfo()
+        for media in ci.media:
+            if (
+                media.type == pj.PJMEDIA_TYPE_AUDIO
+                and media.status == pj.PJSUA_CALL_MEDIA_ACTIVE
+            ):
+                self._media_consented = True
+                break
         return super().onCallMediaState(prm)
-
-    def getInfo(self):
-        """clear flags and do its regular behavior."""
-        self.info_changed = False
-        self.last_info = super().getInfo()
-        return self.last_info
 
 
 class Account(pj.Account):
     def __init__(self):
         super().__init__()
         self._call = None
-        self._call_op_param = None
 
     def onIncomingCall(self, prm):
-        """Handle incoming call."""
         call = Call(self, call_id=prm.callId)
         call_prm = pj.CallOpParam(True)
         call_prm.statusCode = pj.PJSIP_SC_OK
         call.answer(call_prm)
         self._call = call
-        self._call_op_param = call_prm
         return super().onIncomingCall(prm)
