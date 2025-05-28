7c7b89dae7f5766eada388e8b7482419a476c3a0
aj <mohamad.hasan.sohan.ajini@gmail.com>
2025-05-29 00:24:16 +0330

    Initialize attributes

--- src/custom_callbacks.py
+++ src/custom_callbacks.py
@@ -6,6 +6,7 @@ import pjsua2 as pj
 class Call(pj.Call):
     def __init__(self, acc, call_id=pj.PJSUA_INVALID_ID):
         super().__init__(acc, call_id)
+        self._delete_call = False
 
     def onCallState(self, prm):
         """Invoked with call state changes in SIP."""
