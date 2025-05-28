c8bd38cde8cf04d6b0aeed6dcb696cc441e6f411
aj <mohamad.hasan.sohan.ajini@gmail.com>
2025-05-28 23:50:24 +0330

    Remove unnecessary callbacks

--- src/custom_callbacks.py
+++ src/custom_callbacks.py
@@ -6,23 +6,11 @@ import pjsua2 as pj
 class Call(pj.Call):
     def __init__(self, acc, call_id=pj.PJSUA_INVALID_ID):
         super().__init__(acc, call_id)
-        self._delete_call = False
-        self._media_consented = False
 
     def onCallState(self, prm):
         """Invoked with call state changes in SIP."""
         if self.getInfo().state == pj.PJSIP_INV_STATE_DISCONNECTED:
             self._delete_call = True
-
-    def onCallMediaState(self, prm):
-        """Invoked upon call media consent between caller and callee."""
-        # self.media_changed = True
-        # self.last_prm = prm
-        # print("media state changed\n" * 100)
-        for media in self.getInfo().media:
-            if media.type == pj.PJMEDIA_TYPE_AUDIO:
-                self._media_consented = True
-        return super().onCallMediaState(prm)
 
 
 class Account(pj.Account):
