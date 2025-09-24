085c5e42c23b1b30b9667b8de89485d648f6b410
aj <mohamad.hasan.sohan.ajini@gmail.com>
2025-09-24 03:45:49 +0330

    Initial commit

---
+++ src/keyword_update/utils.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+
+from sqlalchemy import update
+
+from database import db_session, init_db
+from models import Keyword, Status
+
+init_db()
+
+
+def get_confirmed_words():
+    confirmed_keywords = [
+        k.word
+        for k in db_session.query(Keyword)
+        .join(Status)
+        .filter(Status.status == "confirmed")
+        .all()
+    ]
+    return confirmed_keywords
+
+
+def get_pending_words():
+    pending_keywords = [
+        k.word
+        for k in db_session.query(Keyword)
+        .join(Status)
+        .filter(Status.status == "pending")
+        .all()
+    ]
+    return pending_keywords
+
+
+def impose_form(form):
+    # print(form)
+    confirmed_at_db = set(get_confirmed_words())
+    pending_at_db = set(get_pending_words())
+    confirmed_at_form = set([k for (k, v) in form.items() if v == "confirmed"])
+    pending_at_form = set([k for (k, v) in form.items() if v == "pending"])
+    # print(confirmed_at_form)
+    # print(pending_at_form)
+    all_words = confirmed_at_db.union(pending_at_db)
+    print("all words", all_words)
+    # update
+    date = datetime.now().date()
+    for word in all_words:
+        # confirmed => pending
+        if word in confirmed_at_db and word not in confirmed_at_form:
+            print("updating", word, "to pending")
+            db_session.execute(
+                update(Keyword)
+                .where(Keyword.word == word)
+                .values(status_id=2, date=date)
+            )
+        # pending => confirmed
+        elif word in pending_at_db and word in pending_at_form:
+            print("updating", word, "to confirmed")
+            db_session.execute(
+                update(Keyword)
+                .where(Keyword.word == word)
+                .values(status_id=1, date=date)
+            )
+    db_session.commit()
